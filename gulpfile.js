3e5c1ad73b31ad4792b98d69f05e47263248d01e
xinwuzu <2212301206@qq.com>
2016-03-08 22:12:48 +0800

    gulp-minify-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
+var minify = require('gulp-minify-css');
 
 
 gulp.task('sass', function(){
 	gulp.src('src/main.scss')
 		.pipe(sass())
 		.pipe(prefix())
+		.pipe(minify())
 		.pipe(gulp.dest('dist/'));
 });
 
